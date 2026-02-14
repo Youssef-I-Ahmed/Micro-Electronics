91f1051937a2e65001c89af3671130a915391c2b
Yousef-I-Ahmed <yousef8720@outlook.com>
2026-02-14 11:39:26 +0200

    first Create Post

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 async function dbConnect() {
   try {
@@ -25,13 +26,24 @@ app.post("/register", async (req, res) => {
   try {
     //get  data
     const{username,email,password,role}=req.body
-  } catch (error) {
-    // validation
-    if (!username || !email || !password || !role) return req.status(400).json({msg: "Invalid Data!!"});
+        // validation
+    if (!username || !email || !password) return req.status(400).json({msg: "Invalid Data!!"});
+
+    const existUser = await User.findOne({email});
+    if (existUser) return res.status(400).json({msg: "Account already Exist"})
     // Create New User
+    const hashPassword = await bcrypt.hash(password,10)
+    const user = await User.create({
+      username, email,
+      password: hashPassword,
+      role
+    })
     // Response
+    res.status(201).json({msg: "Done Created User", data: user,})
+  } catch (error) {
+    console.log(error); 
   }
-})
+});
 
 
 
